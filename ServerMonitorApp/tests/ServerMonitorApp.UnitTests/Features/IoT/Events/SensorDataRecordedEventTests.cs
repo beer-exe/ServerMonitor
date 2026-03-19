1573d986ba0fbf4af90e1437cd54d31919811649
Nghia <nguyenphuocnghia2369@gmail.com>
2026-03-19 16:33:07 +0700

    test(iot): add unit test for SensorDataRecordedEvent
    - Added tests to verify that the constructor correctly initializes all properties (DeviceId, SensorDataId, Temperature, Humidity).
    - Ensured data integrity for the event object before it gets published to handlers.

---
+++ ServerMonitorApp/tests/ServerMonitorApp.UnitTests/Features/IoT/Events/SensorDataRecordedEventTests.cs
@@ -0,0 +1,27 @@
+﻿using ServerMonitorApp.Application.Features.IoT.Events;
+
+namespace ServerMonitorApp.UnitTests.Features.IoT.Events
+{
+    public class SensorDataRecordedEventTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializePropertiesCorrectly()
+        {
+            Guid expectedDeviceId = Guid.NewGuid();
+            long expectedSensorDataId = 9999L;
+            decimal expectedTemperature = 25.4m;
+            decimal expectedHumidity = 60.5m;
+
+            SensorDataRecordedEvent @event = new SensorDataRecordedEvent(
+                expectedDeviceId,
+                expectedSensorDataId,
+                expectedTemperature,
+                expectedHumidity);
+
+            Assert.Equal(expectedDeviceId, @event.DeviceId);
+            Assert.Equal(expectedSensorDataId, @event.SensorDataId);
+            Assert.Equal(expectedTemperature, @event.Temperature);
+            Assert.Equal(expectedHumidity, @event.Humidity);
+        }
+    }
+}
